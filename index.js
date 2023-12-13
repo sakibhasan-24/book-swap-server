b8c0961cc6a28def8ad17f09756af44343fc913a
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-13 16:56:09 +0600

    connect mongoose for database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO);
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example c app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example  app listening on port ${port}!`));
+//
